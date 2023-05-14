d4d1f0c66f995fe70bf982984f85613337fe48c8
Mostafa Zayed <mostafa.zayed9990@gmail.com>
2023-05-14 13:41:52 +0300

    add relationships between models

--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Reply extends Model
 {
     use HasFactory;
+
+    public function question()
+    {
+        return $this->belongsTo(Quetion::class,'quetion_id','id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class,'user_id','id');
+    }
+
+    public function like()
+    {
+        return $this->hasMany(Like::class,'reply_id','id');
+    }
 }
